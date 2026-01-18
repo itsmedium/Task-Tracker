fbfb1b2be10c23baa2858c558f00b8f98aa57abf
Yurii Lukirin <medium12345q@gmail.com>
2026-01-18 17:10:31 +0100

    add: implemented Task class

---
+++ src/task.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from enum import Enum
+import itertools
+
+
+id_generator = itertools.count(1)
+
+
+class Status(Enum):
+    TODO = "todo"
+    IN_PROGRESS = "in_progress"
+    DONE = "done"
+
+
+@dataclass
+class Task:
+    """
+    The class create a task in T0DO list
+    """
+
+    id: int = field(default_factory=lambda: next(id_generator))
+    title: str = "Untitled Task"
+    description: str = " "
+    status: Status = Status.TODO
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime = field(default_factory=datetime.now)
+
+    def mark_updated(self) -> None:
+        """Update the modification timestamp to the current time."""
+        self.updated_at = datetime.now()
+
+    def __post_init__(self) -> None:
+        """Clean up input data after the object is initialized."""
+        self.title = self.title.strip()
