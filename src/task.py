20f8eec26afad8cae95367cb899afea6eb4233db
Yurii Lukirin <medium12345q@gmail.com>
2026-01-19 06:09:35 +0100

    fix: fixed Status validation and change Enum -> StrEnum

--- src/task.py
+++ src/task.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass, field
 from datetime import datetime
-from enum import Enum
+from enum import StrEnum
 import itertools
 
 
 id_generator = itertools.count(1)
 
 
-class Status(Enum):
+class Status(StrEnum):
     TODO = "todo"
     IN_PROGRESS = "in_progress"
     DONE = "done"
@@ -21,10 +21,10 @@ class Task:
 
     id: int = field(default_factory=lambda: next(id_generator))
     title: str = "Untitled Task"
-    description: str = " "
+    description: str = ""
     status: Status = Status.TODO
     created_at: datetime = field(default_factory=datetime.now)
-    updated_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime | None = None
 
     def mark_updated(self) -> None:
         """Update the modification timestamp to the current time."""
@@ -33,3 +33,9 @@ class Task:
     def __post_init__(self) -> None:
         """Clean up input data after the object is initialized."""
         self.title = self.title.strip()
+        self.description = self.description.strip()
+
+        if isinstance(self.status, str):
+            self.status = Status(self.status)
+        elif not isinstance(self.status, Status):
+            raise ValueError("status must be Status enum")
